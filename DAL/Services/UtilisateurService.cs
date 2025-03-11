96921b15be37dc5846154267aaa1c7a2ea541f68
KhaoulaAYARI <khawla.ayyari@gmail.com>
2025-03-11 16:23:25 +0100

    DAL.Entities.Utilisateur debut

---
+++ DAL/Services/UtilisateurService.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Entities;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Services
+{
+    public class UtilisateurService
+    {
+        private const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Database_Epreuve;Integrated Security=True;";
+        public IEnumerable<Utilisateur> Get()
+        {
+            using (SqlConnection connection=new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "";
+                }
+            }
+        }
+    }
+}
