c3aa39d12cc955fdb9a6712aa035441a5a6e4829
KhaoulaAYARI <khawla.ayyari@gmail.com>
2025-03-11 12:21:31 +0100

    Tables Jeu+Utilisateur OK

--- Database/Tables/Jeu.sql
+++ Database/Tables/Jeu.sql
@@ -1,4 +1,17 @@
 ﻿CREATE TABLE [dbo].[Jeu]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Jeu_Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+	[Nom]  NVARCHAR(64) NOT NULL,
+	[Description] NVARCHAR(512) NOT NULL,
+	[AgeMin] INT NOT NULL,
+	[AgeMax] INT NOT NULL,
+	[Nombre_Joueur_Min] INT NOT NULL,
+	[Nombre_Joueur_Max] INT NOT NULL,
+	[Duree_Minute] TIME,
+	[Date_Creation] DATETIME NOT NULL DEFAULT GETDATE(),
+	[Cree_par] UNIQUEIDENTIFIER,
+	CONSTRAINT CK_NbJoueur CHECK ([Nombre_Joueur_Min]<[Nombre_Joueur_Max]),
+	CONSTRAINT CK_AgeJoueur CHECK ([AgeMin]<[AgeMax]),
+	CONSTRAINT FK_UtilisateurCreateur FOREIGN KEY ([Cree_par]) REFERENCES [Utilisateur] ([Id_Utilisateur]) ON DELETE SET NULL
+
 )
