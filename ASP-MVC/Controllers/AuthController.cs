c28644501e5285f9abe573c87f86ab087f5cff78
KhaoulaAYARI <khawla.ayyari@gmail.com>
2025-03-13 11:48:52 +0100

    AuthController/Login ok

---
+++ ASP-MVC/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using ASP_MVC.Models.Auth;
+using BLL.Entities;
+using Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        private IUtilisateurRepository<Utilisateur> _utilisateurService;
+
+        public AuthController(IUtilisateurRepository<Utilisateur> utilisateurService)
+        {
+            _utilisateurService = utilisateurService;
+        }
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(Login));
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(AuthLoginForm form)
+        {
+            try
+            {
+                if (!ModelState.IsValid) throw new ArgumentException(nameof(form));
+                Guid id = _utilisateurService.CheckPassword(form.Email, form.password);
+                return RedirectToAction("Details", "Utilisateur", new { id = id });
+            }
+            catch (Exception)
+            {
+
+                return View();
+            }
+        }
+    }
+}
---
+++ ASP-MVC/Models/Auth/AuthLoginForm.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ASP_MVC.Models.Auth
+{
+    public class AuthLoginForm
+    {
+        [DisplayName("Email: ")]
+        [Required(ErrorMessage = "Le champ (Email) est obligatoire")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+
+        [DisplayName("Mot de passe: ")]
+        [Required(ErrorMessage = "Le champ (Mot de passe) est obligatoire")]
+        [MaxLength(32, ErrorMessage = "Le champ (Mot de passe) doit contenier au maximum 32 caracteres")]
+        [MinLength(8, ErrorMessage = "Le champ (Mot de passe) doit contenier au minimum 8 caracteres")]
+        [RegularExpression(@"^.*(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\-_+=\.()\[\]$µ£\\/*§?{}]).*", ErrorMessage = "Le champ 'Mot de passe' doit contenir au minimum une minuscule, une majuscule, un chiffre et un symbole.")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+}
